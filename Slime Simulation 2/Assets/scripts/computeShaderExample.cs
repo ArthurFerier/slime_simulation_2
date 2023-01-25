150b91e1700be5f8183743621a1d842dfa4c03f3
Arthur <arthur.ferier1@gmail.com>
2023-01-25 23:15:15 +0100

    compute shader appears on screen 2

---
+++ Slime Simulation 2/Assets/scripts/computeShaderExample.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class computeShaderExample : MonoBehaviour
+{
+
+    public ComputeShader computeShader;
+
+    private RenderTexture _src;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (_src == null)
+        {
+            _src = new RenderTexture(1920, 1080, 24);
+            _src.enableRandomWrite = true;
+            _src.Create();
+        }
+        
+        computeShader.SetTexture(0, "Result", _src);
+        // tells the shader which kernel to dispatch and how many thread groups to use
+        // so we divide our image size by our thread size of each group
+        computeShader.Dispatch(0, _src.width/8, _src.height/8, 1);
+        
+        Graphics.Blit(_src, dest);
+    }
+}
