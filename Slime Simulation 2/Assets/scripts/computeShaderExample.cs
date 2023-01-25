92745c2da5f3f3f8f5ebfdd7af65aea92e9bd30e
Arthur <arthur.ferier1@gmail.com>
2023-01-25 23:37:47 +0100

    can make random noise appear at the screen
     but problems of blurred image when not 1 square per pixel

--- Slime Simulation 2/Assets/scripts/computeShaderExample.cs
+++ Slime Simulation 2/Assets/scripts/computeShaderExample.cs
@@ -14,12 +14,12 @@ public class computeShaderExample : MonoBehaviour
     {
         if (_src == null)
         {
-            _src = new RenderTexture(1920, 1080, 24);
+            _src = new RenderTexture(160, 90, 24);
             _src.enableRandomWrite = true;
             _src.Create();
         }
         
-        computeShader.SetTexture(0, "Result", _src);
+        computeShader.SetTexture(0, "Texture", _src);
         // tells the shader which kernel to dispatch and how many thread groups to use
         // so we divide our image size by our thread size of each group
         computeShader.Dispatch(0, _src.width/8, _src.height/8, 1);
